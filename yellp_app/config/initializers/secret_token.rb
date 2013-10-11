5c9d38ececfcb7703394ea30965e5acec6780106
xiaoting wright <haley.xt.wright@gmail.com>
2013-10-11 10:22:51 +0100

    set up rails app

---
+++ yellp_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YellpApp::Application.config.secret_key_base = 'b19e84bd6e6e7e2ae577de79a4d72d2728611f1f746e83a6929243e3231365f698fba9ea0105ecd498f029f5e788e2c02c4fc2af635982b666916b9b44653e50'
---
+++ yellp_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YellpApp::Application.config.session_store :cookie_store, key: '_yellp_app_session'
